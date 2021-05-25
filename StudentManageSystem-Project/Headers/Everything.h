1fda58d00c7b443f59289c563ac5781acf3b99df
happysnaker <1637318597@qq.com>
2021-05-25 17:39:07 +0800

    重大更新，详细添加了注释

---
+++ StudentManageSystem-Project/Headers/Everything.h
@@ -0,0 +1,16 @@
+#ifndef EVERTTHING_H
+#define EVERTTHING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "ShowMap.h"
+#include <conio.h>
+#include <stdbool.h>
+#include "FileO.h"
+#include "Core1.h"
+#include "Core2.h"
+#include "Input.h"
+
+
+#endif // EVERTTHING_H
